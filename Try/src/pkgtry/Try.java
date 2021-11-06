b1e2a1528796c432b55cbac7ee7644b30c3b0ae8
Luis <luis@ngel.com>
2021-11-05 19:49:06 -0600

    prueba subida en consola

---
+++ Try/src/pkgtry/Try.java
@@ -0,0 +1,18 @@
+
+package pkgtry;
+
+/**
+ *
+ * @author LUIS
+ */
+public class Try {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola git");
+    }
+    
+}
